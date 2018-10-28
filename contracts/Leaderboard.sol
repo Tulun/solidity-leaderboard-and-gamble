716872325a074aea29b543f2e5d7cba3635c4781
Jason Kiraly <jaykiraly@gmail.com>
2018-10-28 10:58:15 -0700

    Initial commit of Leaderboard from remix.

---
+++ contracts/Leaderboard.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.24;
+
+contract Leaderboard {
+  // initialize variables
+  struct Player {
+      string name;
+      address playerAddress;
+      uint wins;
+      uint losses;
+      uint ties;
+      uint numDisputedGames;
+  }
+  
+  Player[] public players;
+  address private owner;
+  mapping(address => bool) public playersAdded;
+  
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+  
+  constructor() public {
+      owner = msg.sender;
+  }
+  
+  function addPlayer(string name) public {
+      // Make sure this particular address hasn't been added yet.
+      require(!playersAdded[msg.sender]);
+      
+      playersAdded[msg.sender] = true;
+      Player memory newPlayer = Player({
+          name: name,
+          playerAddress: msg.sender,
+          wins: 0,
+          losses: 0,
+          ties: 0,
+          numDisputedGames: 0
+      });
+      
+      players.push(newPlayer);
+  }
+}
