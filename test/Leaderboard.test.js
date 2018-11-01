db05c0d99dc8167ed4357efc793ff51e6e36a449
Jason Kiraly <jaykiraly@gmail.com>
2018-11-01 13:46:52 -0700

    refactoring tests a bit.

--- test/Leaderboard.test.js
+++ test/Leaderboard.test.js
@@ -24,179 +24,170 @@ describe("Leaderboard", () => {
     console.log(leaderboard)
   });
 
-  it("Adds new players to the contract", async () => {
-    await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
-      from: accounts[0],
-      gas: '1000000'
-    });
-    
-    await leaderboard.methods.addPlayerToLeaderboard("George").send({
-      from: accounts[1],
-      gas: '1000000'
-    });
-
-    const p1 = await leaderboard.methods.players(0).call();
-    const p2 = await leaderboard.methods.players(1).call();
-    assert.equal(p1.name, "Jason");
-    assert.equal(p2.name, "George");
-
-    const p1Added = await leaderboard.methods.playersAdded(p1.playerAddress);
-    const p2Added = await leaderboard.methods.playersAdded(p2.playerAddress);
-
-    assert(p1Added);
-    assert(p2Added);
-
-    const totalNumPlayers = await leaderboard.methods.totalNumPlayers().call();
-
-    assert.equal(totalNumPlayers, 2);
-  });
-  
-
-  it("Prevents a new player from being added if their address exists", async () => {
-    await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
-      from: accounts[0],
-      gas: '1000000'
-    });
-
-    try {
+  describe("Adding players", () => {
+    beforeEach( async () => {
       await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
         from: accounts[0],
         gas: '1000000'
       });
-      assert.fail("Second player was added when it shouldn't.")
-    } 
-    catch (err) {
-      assert(err);
-    };
+      
+      await leaderboard.methods.addPlayerToLeaderboard("George").send({
+        from: accounts[1],
+        gas: '1000000'
+      });
+    });
+
+    it("Adds new players to the contract", async () => {
+
+      const p1 = await leaderboard.methods.players(0).call();
+      const p2 = await leaderboard.methods.players(1).call();
+      assert.equal(p1.name, "Jason");
+      assert.equal(p2.name, "George");
+  
+      const p1Added = await leaderboard.methods.playersAdded(p1.playerAddress);
+      const p2Added = await leaderboard.methods.playersAdded(p2.playerAddress);
+  
+      assert(p1Added);
+      assert(p2Added);
+  
+      const totalNumPlayers = await leaderboard.methods.totalNumPlayers().call();
+  
+      assert.equal(totalNumPlayers, 2);
+    });
+    
+  
+    it("Prevents a new player from being added if their address exists", async () => {
+      try {
+        await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
+          from: accounts[0],
+          gas: '1000000'
+        });
+        assert.fail("Second player was added when it shouldn't.")
+      } 
+      catch (err) {
+        assert(err);
+      };
+    });
   });
 
-  it("Can create a game", async () => {
-    await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
-      from: accounts[0],
-      gas: '1000000'
+  describe("It can create a game", () => {
+    beforeEach( async () => { 
+      await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+
+      await leaderboard.methods.addPlayerToLeaderboard("George").send({
+        from: accounts[1],
+        gas: '1000000'
+      });
+
+      await leaderboard.methods.addPlayerToLeaderboard("Tim").send({
+        from: accounts[2],
+        gas: '1000000'
+      });
     });
 
-    await leaderboard.methods.createGame().send({
-      from: accounts[0],
-      gas: "1000000",
-      value: web3.utils.toWei("0.1", "ether")
-    })
-
-    const game = await leaderboard.methods.game().call();
-    assert.equal(game.id, 1);
-    assert.equal(game.firstPlayer, accounts[0]);
-    assert.equal(game.secondPlayer, NULL_ADDRESS);
-    assert.equal(game.bet, web3.utils.toWei("0.1", "ether"));
-    assert.equal(game.pot, web3.utils.toWei("0.1", "ether"));
-    assert.equal(game.winner, NULL_ADDRESS);
-  });
-
-  it("prevents a second game from being created while one exists", async () => {
-    await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
-      from: accounts[0],
-      gas: '1000000'
-    });
-
-    await leaderboard.methods.createGame().send({
-      from: accounts[0],
-      gas: "1000000",
-      value: web3.utils.toWei("0.1", "ether")
-    });
-
-    try {
+    it("Can create a game", async () => {
       await leaderboard.methods.createGame().send({
         from: accounts[0],
         gas: "1000000",
         value: web3.utils.toWei("0.1", "ether")
       })
-      assert.fail("Second game call was successful when it should throw.");
-    }
-    catch (err) {
-      assert(err);
-    }
-  });
+  
+      const game = await leaderboard.methods.game().call();
+      assert.equal(game.id, 1);
+      assert.equal(game.firstPlayer, accounts[0]);
+      assert.equal(game.secondPlayer, NULL_ADDRESS);
+      assert.equal(game.bet, web3.utils.toWei("0.1", "ether"));
+      assert.equal(game.pot, web3.utils.toWei("0.1", "ether"));
+      assert.equal(game.winner, NULL_ADDRESS);
+    });
+  })
+  
+  describe("Players interacting with game", () => {
+    beforeEach( async() => {
+      await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
+        from: accounts[0],
+        gas: '1000000'
+      });
 
-  it("prevents a user from creating a game if they haven't registered on the board", async () => {
-    try {
+      await leaderboard.methods.addPlayerToLeaderboard("George").send({
+        from: accounts[1],
+        gas: '1000000'
+      });
+
+      await leaderboard.methods.addPlayerToLeaderboard("Tim").send({
+        from: accounts[2],
+        gas: '1000000'
+      });
+
       await leaderboard.methods.createGame().send({
         from: accounts[0],
         gas: "1000000",
         value: web3.utils.toWei("0.1", "ether")
-      });
-      assert.fail("Game was created without a user registering.");
-    }
-    catch (err) {
-      assert(err);
-    }
-  })
-
-  it("Add a second player to a game", async () => {
-    await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
-      from: accounts[0],
-      gas: '1000000'
+      })
     });
 
-    await leaderboard.methods.addPlayerToLeaderboard("George").send({
-      from: accounts[1],
-      gas: '1000000'
+    it("prevents a second game from being created while one exists", async () => {
+      try {
+        await leaderboard.methods.createGame().send({
+          from: accounts[0],
+          gas: "1000000",
+          value: web3.utils.toWei("0.1", "ether")
+        })
+        assert.fail("Second game call was successful when it should throw.");
+      }
+      catch (err) {
+        assert(err);
+      }
     });
 
-    await leaderboard.methods.createGame().send({
-      from: accounts[0],
-      gas: "1000000",
-      value: web3.utils.toWei("0.1", "ether")
+    it("prevents a user from creating a game if they haven't registered on the board", async () => {
+      try {
+        await leaderboard.methods.createGame().send({
+          from: accounts[3],
+          gas: "1000000",
+          value: web3.utils.toWei("0.1", "ether")
+        });
+        assert.fail("Game was created without a user registering.");
+      }
+      catch (err) {
+        assert(err);
+      }
     });
 
-    await leaderboard.methods.addSecondPlayerToGame().send({
-      from: accounts[1],
-      gas: "1000000",
-      value: web3.utils.toWei("0.1", "ether")
-    })
-
-    const game = await leaderboard.methods.game().call();
-    assert.equal(game.secondPlayer, accounts[1]);
-    assert.equal(game.pot, web3.utils.toWei("0.2", "ether"));
-  })
-
-  it("Prevents a 3rd player from entering the game", async () => {
-    await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
-      from: accounts[0],
-      gas: '1000000'
-    });
-
-    await leaderboard.methods.addPlayerToLeaderboard("George").send({
-      from: accounts[1],
-      gas: '1000000'
-    });
-
-    await leaderboard.methods.addPlayerToLeaderboard("Tim").send({
-      from: accounts[2],
-      gas: '1000000'
-    });
-
-    await leaderboard.methods.createGame().send({
-      from: accounts[0],
-      gas: "1000000",
-      value: web3.utils.toWei("0.1", "ether")
-    });
-
-    await leaderboard.methods.addSecondPlayerToGame().send({
-      from: accounts[1],
-      gas: "1000000",
-      value: web3.utils.toWei("0.1", "ether")
-    })
-
-    try {
+    it("Add a second player to a game", async () => {
       await leaderboard.methods.addSecondPlayerToGame().send({
-        from: accounts[2],
+        from: accounts[1],
         gas: "1000000",
         value: web3.utils.toWei("0.1", "ether")
-      });
-      assert.fail("addSecondPlayerToGame call didn't throw an error, although a third player tried to come in.");
-    }
-    catch (err) {
-      assert(err);
-    }
+      })
+  
+      const game = await leaderboard.methods.game().call();
+      assert.equal(game.secondPlayer, accounts[1]);
+      assert.equal(game.pot, web3.utils.toWei("0.2", "ether"));
+    })
+
+    it("Prevents a 3rd player from entering the game", async () => {
+      await leaderboard.methods.addSecondPlayerToGame().send({
+        from: accounts[1],
+        gas: "1000000",
+        value: web3.utils.toWei("0.1", "ether")
+      })
+  
+      try {
+        await leaderboard.methods.addSecondPlayerToGame().send({
+          from: accounts[2],
+          gas: "1000000",
+          value: web3.utils.toWei("0.1", "ether")
+        });
+        assert.fail("addSecondPlayerToGame call didn't throw an error, although a third player tried to come in.");
+      }
+      catch (err) {
+        assert(err);
+      }
+    })
   })
 
   it("Forces a user to send the correct bet value", async  () => {
@@ -702,6 +693,42 @@ describe("Leaderboard", () => {
 
     const gameInProgress = await leaderboard.methods.gameInProgress().call();
     assert.equal(gameInProgress, false);
+  });
+
+  it("Refunds the user in the event of a tie", async () => {
+    await leaderboard.methods.addPlayerToLeaderboard("Jason").send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await leaderboard.methods.addPlayerToLeaderboard("George").send({
+      from: accounts[1],
+      gas: '1000000'
+    });
+
+    await leaderboard.methods.createGame().send({
+      from: accounts[0],
+      gas: "1000000",
+      value: web3.utils.toWei("1", "ether")
+    });
+
+    await leaderboard.methods.addSecondPlayerToGame().send({
+      from: accounts[1],
+      gas: "1000000",
+      value: web3.utils.toWei("1", "ether")
+    });
+
+    await leaderboard.methods.chooseWinner("tie").send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+    await leaderboard.methods.chooseWinner("tie").send({
+      from: accounts[1],
+      gas: "1000000",
+    });
+
+
   })
 
   it("If both members disagree on outcome, they get a disputed outcome added to their stats", async () => {
